9dac466a8fb88c710a43245c2ba6ad2ff5af6750
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-12 13:45:08 +0200

    fin ex des numéros en php, manque couleur en js

---
+++ index2.php
@@ -0,0 +1,44 @@
+<!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>punition</title>
+    </head>
+
+    <body>
+      <?php
+      //écrire un texte
+        //echo "Je suis une perruche";
+        
+
+      //l'écrire 1500 fois
+      /*$x = 0;
+      while ($x <= 1500){
+        echo "Je suis une perruches <br>";
+        $x++;
+      }*/
+
+      //l'écrire avec numérotation
+      /*for($y = 0; $y <= 1500; $y++){
+        echo "$y- je suis une perruche <br>";
+      }*/
+
+      //nombres impairs jusque 20
+      /*$n = 1;
+      while($n <= 20){
+        echo "$n<br>";
+        $n = $n + 2;
+      }*/
+
+      //nombres pairs jusque 100000
+      $m = 0;
+      while($m <= 100000){
+        echo "$m<br>";
+        $m = $m + 2;
+      }
+
+
+       ?>
+
+    </body>
+  </html>
